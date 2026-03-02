352272c43207e600a6114a7b19b3daba8228cc14
Tanel Pauls <paulstanel@protonmail.com>
2026-03-02 10:40:15 +0200

    03 - data types

--- course_0001_hello_world/0001_hello.rs
+++ course_0001_hello_world/0001_hello.rs
@@ -1,5 +1,3 @@
 fn main() {
     println!("Hello, world!");
 }
-
-// start: open terminal, then "rustc filename" then it will run the code and create exe file
--- course_0003_data_types/src/main.rs
+++ course_0003_data_types/src/main.rs
@@ -1,3 +1,49 @@
+//primitive data types
+// int, float, boo´l, char
+
+// INTEGER
+// Rust has signred (+ and -) and unsignred
+//integer (only+) types of different sizes.
+
+//i8, i16, i32, i64, i128: Signed integers.
+//u8, u16, u32, u64, u128: Signed integers.
+
 fn main() {
-    println!("Hello, world!");
+    let x: i32 = -42;
+    let y: u64 = 100;
+    //let y: u64 = -100; <- this will end up in compile error.
+    println!("Signed Integer: {}", x);
+    println!("Unsigned Integer: {}", y);
+
+
+// diff bet i32 (32 bits) and u64 (64 bits)
+// range: i32 (-2**31 +2**31)
+// i32 - 2147483647
+// i64 - 9223372036854775807
+    let mut e: i128 = 2147483647; // with i/32 it was too small and created an error, so I had to increase it
+    let i: i64 = 9223372036854775807;
+    println!("Maximum value of i32: {}", e);
+    println!("Maximum value of i64: {}", i);
+    e = e + 1;
+    println!("e + 1= : {}", e);
+
+
+// Floats [Floating Point Types]
+// f32, f64
+    let pi: f64 = 3.14;
+    println!("Value of pi: {}", pi);
+
+// Boolean Values: true, false
+
+    let is_snowing: bool = true;
+
+    println!("Is it snowing ? - {}", is_snowing);
+
+// Character Type - char
+
+    let letter: char = 'a';
+
+    println!("First letter of the alphabey: {}", letter);
+
+
 }
